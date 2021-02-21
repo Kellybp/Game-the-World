d5c1a0ec69ff22a70197b94e84662a16e656b8d5
Kellybp <indeterminatarum@gmail.com>
2021-02-21 09:08:50 -0500

    Finished adding basic styling to Register.js and login.js

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -82,7 +82,6 @@ const Homepage = () => {
     getUserPosition();
     getNextEventPage();
   }, []);
-  // <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   return (
       <Layout>
         {/*<Header>*/}
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import {authMethods} from '../firebase/auth';
 import { useHistory } from 'react-router-dom';
 import { Form, Input, Button, Checkbox } from 'antd';
+import '../styling/registrationStyle.css'
+import { Divider, Row, Col, Typography } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 const Login = () => {
     let history = useHistory();
@@ -21,18 +24,67 @@ const Login = () => {
                 alert(e.message);
             });
     };
+    const tailLayout = {
+        wrapperCol: { offset: 4, span: 16 },
+    };
 
     return(
-    <div>
-        LOGIN
-        <form>
-            <label htmlFor="email">Email</label>
-            <input value={username} onChange={e => setUsername(e.target.value)} type="email"  placeholder="Username" name="email"/>
-            <label htmlFor="password">Password</label>
-            <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" name="password"/>
-            <button type="button" onClick={handleSubmit}> Login</button>
-        </form>
-    </div>
+        <Row>
+            <Col span={16} id="image">
+
+            </Col>
+            <Col span={8} id="form">
+                <Form
+                    name="basic"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                >
+                    <Typography.Title>LOGIN</Typography.Title>
+                    <Divider />
+                    <br/>
+                    <Form.Item
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email'}]}
+                    >
+                        <Input
+                            prefix={<UserOutlined className="site-form-item-icon" />}
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            type="email"
+                            placeholder="Username"
+                            name="email"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                            Login
+                        </Button>
+                    </Form.Item>
+
+                </Form>
+            </Col>
+        </Row>
     );
 }
 
